de4f0633b26c331e3dac68e4158aafd70d2a259c
Prince Bioh <princebioh15@gmail.com>
2023-07-19 08:36:32 +0000

    Refactor date module

--- date.js
+++ date.js
@@ -1,14 +1,13 @@
 
 module.exports = function() {
     const today = new Date();
+
     const  options = {
         day: "numeric",
         weekday: "long",
         month: "long"
     };
 
-    const day = today.toLocaleDateString("en-US", options);
-
-    return day;
-    
+    return today.toLocaleDateString("en-US", options);
+ 
 }
